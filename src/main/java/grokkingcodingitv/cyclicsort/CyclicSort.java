1c6ca08c0a8653da5826259bfb4c2b18887e7aab
hungpc <hungpc@sendo.vn>
2020-11-26 17:20:34 +0700

    cyclic sort

---
+++ src/main/java/grokkingcodingitv/cyclicsort/CyclicSort.java
@@ -0,0 +1,16 @@
+package grokkingcodingitv.cyclicsort;
+
+public class CyclicSort {
+
+  /**
+   * sort 1-> n
+   * Input: [1, 5, 6, 4, 3, 2]
+   * Output: [1, 2, 3, 4, 5, 6]
+   */
+
+  public int[] cyclicSort(int[] nums) {
+
+    return nums;
+
+  }
+}
