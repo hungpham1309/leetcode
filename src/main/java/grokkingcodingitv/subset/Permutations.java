5799f29f77c646b7e62e03c70b493947048fec02
hungpc <hungpc@sendo.vn>
2020-11-26 16:24:00 +0700

    subset

--- src/main/java/grokkingcodingitv/subset/Permutations.java
+++ src/main/java/grokkingcodingitv/subset/Permutations.java
@@ -1,7 +1,9 @@
 package grokkingcodingitv.subset;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Permutations {
 
@@ -10,18 +12,52 @@ public class Permutations {
    */
   public List<List<Integer>> findAllPermutations(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
-    List<List<Integer>> permutations = new ArrayList<>();
-    permutations.add(new ArrayList<>());
-    for (int i = 0; i < nums.length; i++) {
-      int size = permutations.size();
-      for (int j = 0; j < size; j++) {
-        for (int k = 0 ; k <= permutations.size(); k ++) {
 
+    Queue<List<Integer>> permutations = new LinkedList<>();
+    permutations.add(new ArrayList<>());
+
+    for (int num : nums) {
+      int size = permutations.size();
+      for (int i = 0; i < size; i++) {
+        List<Integer> oldPermutation = permutations.poll();
+        for (int j = 0; j <= oldPermutation.size(); j++) {
+          List<Integer> newPermutation = new ArrayList<>(oldPermutation);
+          newPermutation.add(j, num);
+
+          if (newPermutation.size() == nums.length) {
+            result.add(newPermutation);
+          } else {
+            permutations.add(newPermutation);
+          }
         }
       }
     }
 
-//    result.addAll(permutations);
+    return result;
+  }
+
+  public List<List<Character>> findAllPermutations(char[] chars) {
+    List<List<Character>> result = new ArrayList<>();
+
+    Queue<List<Character>> permutations = new LinkedList<>();
+    permutations.add(new ArrayList<>());
+
+    for (char num : chars) {
+      int size = permutations.size();
+      for (int i = 0; i < size; i++) {
+        List<Character> oldPermutation = permutations.poll();
+        for (int j = 0; j <= oldPermutation.size(); j++) {
+          List<Character> newPermutation = new ArrayList<>(oldPermutation);
+          newPermutation.add(j, num);
+
+          if (newPermutation.size() == chars.length) {
+            result.add(newPermutation);
+          } else {
+            permutations.add(newPermutation);
+          }
+        }
+      }
+    }
 
     return result;
   }
--- src/test/java/grokkingcodingitv/subset/PermutationsTest.java
+++ src/test/java/grokkingcodingitv/subset/PermutationsTest.java
@@ -20,13 +20,62 @@ public class PermutationsTest {
     var actual = permutations.findAllPermutations(new int[]{1,3,5});
 
     List<List<Integer>> expected = new ArrayList<>();
-    expected.add(List.of(1,3,5));
-    expected.add(List.of(1,5,3));
-    expected.add(List.of(3,1,5));
-    expected.add(List.of(3,5,1));
-    expected.add(List.of(5,1,3));
-    expected.add(List.of(5,3,1));
+    expected.add(List.of(5, 3, 1));
+    expected.add(List.of(3, 5, 1));
+    expected.add(List.of(3, 1, 5));
+    expected.add(List.of(5, 1, 3));
+    expected.add(List.of(1, 5, 3));
+    expected.add(List.of(1, 3, 5));
 
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+
+  /**
+   * Input: [1,3,5]
+   * Output: [1,3,5], [1,5,3], [3,1,5], [3,5,1], [5,1,3], [5,3,1]
+   */
+  @Test
+  public void shouldReturnResultWhenFindPermutation2() {
+    var actual = permutations.findAllPermutations(new int[]{1});
+
+    List<List<Integer>> expected = new ArrayList<>();
+    expected.add(List.of(1));
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  /**
+   * Input: [1,3]
+   * Output: [1,3], [3,1]
+   */
+  @Test
+  public void shouldReturnResultWhenFindPermutation3() {
+    var actual = permutations.findAllPermutations(new int[]{1,3});
+
+    List<List<Integer>> expected = new ArrayList<>();
+    expected.add(List.of(3,1));
+    expected.add(List.of(1,3));
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  /**
+   * Input: [A,B,C]
+   * Output: [A,B,C], [A,C,B], [B,A,C], [B,C,A], [C,A,B], [C,B,A]
+   */
+  @Test
+  public void shouldReturnResultWhenFindPermutation4() {
+    var actual = permutations.findAllPermutations(new char[]{'A','B','C'});
+
+    List<List<Character>> expected = new ArrayList<>();
+    expected.add(List.of('C', 'B', 'A'));
+    expected.add(List.of('B', 'C', 'A'));
+    expected.add(List.of('B', 'A', 'C'));
+    expected.add(List.of('C', 'A', 'B'));
+    expected.add(List.of('A', 'C', 'B'));
+    expected.add(List.of('A', 'B', 'C'));
 
     assertThat(actual).isEqualTo(expected);
   }
